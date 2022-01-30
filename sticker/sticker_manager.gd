677c2a9bc3e31d6bf3f6d69c787d9a5ce9414318
KoryB <byrnek@mymail.shawnee.edu>
2022-01-30 15:41:45 -0500

    Actual MVP, fixed so now the image downloads using JS plugin, verrrrry slow though

--- sticker/sticker_manager.gd
+++ sticker/sticker_manager.gd
@@ -12,15 +12,19 @@ func _process(delta: float):
 	if Input.is_action_just_pressed("screenshot_time"):
 		take_screenshot()
 		
-		# Just in case.........
-		for i in range(10):
-			yield(get_tree(), "idle_frame")
-		
-		$FileDialog.popup_centered()
+		if OS.get_name() == "HTML5" and OS.has_feature('JavaScript'):
+			HTML5File.save_image(screenshot, "your_lovely_creation.png")
+			
+		else:
+			# Just in case.........
+			for i in range(10):
+				yield(get_tree(), "idle_frame")
+			
+			$FileDialog.popup_centered()
 
 
 func _on_FileDialog_file_selected(path):
-	save_screenshot(path)
+	save_screenshot_native(path)
 	$FileDialog.hide()
 		
 		
@@ -29,5 +33,9 @@ func take_screenshot():
 	screenshot.flip_y()
 	
 
-func save_screenshot(path):
+func save_screenshot_html5():
+	HTML5File.save_image(screenshot, "your_lovely_creation.png")
+
+
+func save_screenshot_native(path):
 	screenshot.save_png(path)
